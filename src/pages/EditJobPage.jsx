3fad0a3ca974f6fd31666ae86de2fdc496e89e22
Gregory Harris <63122765+gregoryharris869@users.noreply.github.com>
2024-03-30 01:19:10 -0500

    Proxy needs to be added.

--- src/pages/EditJobPage.jsx
+++ src/pages/EditJobPage.jsx
@@ -1,23 +1,29 @@
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useParams, useLoaderData, useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 
-const AddJobPage = ({ addJobSubmit }) => {
-  const [title, setTitle] = useState("");
-  const [type, setType] = useState("Full-Time");
-  const [location, setLocation] = useState("");
-  const [description, setDescription] = useState("");
-  const [salary, setSalary] = useState("Under $50K");
-  const [companyName, setCompanyName] = useState("");
-  const [companyDescription, setCompanyDescription] = useState("");
-  const [contactEmail, setContactEmail] = useState("");
-  const [contactPhone, setContactPhone] = useState("");
+const EditJobPage = ({ updateJobSubmit }) => {
+  const job = useLoaderData();
+  const [title, setTitle] = useState(job.title);
+  const [type, setType] = useState(job.type);
+  const [location, setLocation] = useState(job.location);
+  const [description, setDescription] = useState(job.description);
+  const [salary, setSalary] = useState(job.salary);
+  const [companyName, setCompanyName] = useState(job.company.name);
+  const [companyDescription, setCompanyDescription] = useState(
+    job.company.description
+  );
+  const [contactEmail, setContactEmail] = useState(job.company.contactEmail);
+  const [contactPhone, setContactPhone] = useState(job.company.contactPhone);
 
   const navigate = useNavigate();
+  const { id } = useParams();
 
   const submitForm = (e) => {
     e.preventDefault();
-    const newJob = {
+
+    const updatedJob = {
+      id,
       title,
       type,
       location,
@@ -30,9 +36,12 @@ const AddJobPage = ({ addJobSubmit }) => {
         contactPhone,
       },
     };
-    addJobSubmit(newJob);
-    toast.success("Job added successfully");
-    return navigate("/jobs");
+
+    updateJobSubmit(updatedJob);
+
+    toast.success("Job Updated Successfully");
+
+    return navigate(`/jobs/${id}`);
   };
 
   return (
@@ -40,7 +49,9 @@ const AddJobPage = ({ addJobSubmit }) => {
       <div className="container max-w-2xl py-24 m-auto">
         <div className="px-6 py-8 m-4 mb-4 bg-white border rounded-md shadow-md md:m-0">
           <form onSubmit={submitForm}>
-            <h2 className="mb-6 text-3xl font-semibold text-center">Add Job</h2>
+            <h2 className="mb-6 text-3xl font-semibold text-center">
+              Update Job
+            </h2>
 
             <div className="mb-4">
               <label
@@ -221,7 +232,7 @@ const AddJobPage = ({ addJobSubmit }) => {
                 className="w-full px-4 py-2 font-bold text-white bg-indigo-500 rounded-full hover:bg-indigo-600 focus:outline-none focus:shadow-outline"
                 type="submit"
               >
-                Add Job
+                Update Job
               </button>
             </div>
           </form>
@@ -230,5 +241,4 @@ const AddJobPage = ({ addJobSubmit }) => {
     </section>
   );
 };
-
-export default AddJobPage;
+export default EditJobPage;
--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -1,9 +1,26 @@
-import { useParams, useLoaderData } from "react-router-dom";
+import { useParams, useLoaderData, useNavigate } from "react-router-dom";
 import { FaArrowLeft, FaMapMarker } from "react-icons/fa";
 import { Link } from "react-router-dom";
-const JobPage = () => {
+import { toast } from "react-toastify";
+
+const JobPage = ({ deleteJob }) => {
   const { id } = useParams();
   const job = useLoaderData();
+  const navigate = useNavigate();
+
+  const onDeleteClick = (jobId) => {
+    const confirm = window.confirm(
+      "Are you sure you want to delete this listing?"
+    );
+
+    if (!confirm) return;
+
+    deleteJob(jobId);
+
+    toast.success("Job deleted successfully");
+
+    navigate("/jobs");
+  };
 
   return (
     <>
@@ -13,20 +30,20 @@ const JobPage = () => {
             to="/jobs"
             className="flex items-center text-indigo-500 hover:text-indigo-600"
           >
-            <FaArrowLeft className="w-4 h-4 mr-2" /> Back to Job Listings
+            <FaArrowLeft className="mr-2" /> Back to Job Listings
           </Link>
         </div>
       </section>
 
       <section className="bg-indigo-50">
         <div className="container px-6 py-10 m-auto">
-          <div className="grid w-full grid-cols-1 gap-6 md:grid-cols-70/30">
+          <div className="grid w-full grid-cols-1 gap-8 md:grid-cols-2 ">
             <main>
               <div className="p-6 text-center bg-white rounded-lg shadow-md md:text-left">
                 <div className="mb-4 text-gray-500">{job.type}</div>
                 <h1 className="mb-4 text-3xl font-bold">{job.title}</h1>
                 <div className="flex justify-center mb-4 text-gray-500 align-middle md:justify-start">
-                  <FaMapMarker className="mr-1 text-orange-700 " />
+                  <FaMapMarker className="mr-1 text-orange-700" />
                   <p className="text-orange-700">{job.location}</p>
                 </div>
               </div>
@@ -42,13 +59,12 @@ const JobPage = () => {
                   Salary
                 </h3>
 
-                <p className="mb-4"> {job.salary} / Year</p>
+                <p className="mb-4">{job.salary} / Year</p>
               </div>
             </main>
 
             {/* <!-- Sidebar --> */}
-            <aside>
-              {/* <!-- Company Info --> */}
+            <aside className="">
               <div className="p-6 bg-white rounded-lg shadow-md">
                 <h3 className="mb-6 text-xl font-bold">Company Info</h3>
 
@@ -58,7 +74,7 @@ const JobPage = () => {
 
                 <hr className="my-4" />
 
-                <h3 className="text-xl">{job.company.location}</h3>
+                <h3 className="text-xl">Contact Email:</h3>
 
                 <p className="p-2 my-2 font-bold bg-indigo-100">
                   {job.company.contactEmail}
@@ -67,20 +83,23 @@ const JobPage = () => {
                 <h3 className="text-xl">Contact Phone:</h3>
 
                 <p className="p-2 my-2 font-bold bg-indigo-100">
+                  {" "}
                   {job.company.contactPhone}
                 </p>
               </div>
 
-              {/* <!-- Manage --> */}
               <div className="p-6 mt-6 bg-white rounded-lg shadow-md">
                 <h3 className="mb-6 text-xl font-bold">Manage Job</h3>
                 <Link
-                  to={`/edit-job/${id}`}
+                  to={`/edit-job/${job.id}`}
                   className="block w-full px-4 py-2 mt-4 font-bold text-center text-white bg-indigo-500 rounded-full hover:bg-indigo-600 focus:outline-none focus:shadow-outline"
                 >
                   Edit Job
                 </Link>
-                <button className="block w-full px-4 py-2 mt-4 font-bold text-white bg-red-500 rounded-full hover:bg-red-600 focus:outline-none focus:shadow-outline">
+                <button
+                  onClick={() => onDeleteClick(job.id)}
+                  className="block w-full px-4 py-2 mt-4 font-bold text-white bg-red-500 rounded-full hover:bg-red-600 focus:outline-none focus:shadow-outline"
+                >
                   Delete Job
                 </button>
               </div>
